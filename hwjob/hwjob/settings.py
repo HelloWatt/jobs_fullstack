a81d1166eb0c518cf66640191b72754278b0af39
Jérôme BON <jerome@hellowatt.fr>
2023-06-06 14:46:19 +0200

    Update deps
    Former-commit-id: cf9ac7e4bb4fc2227c94be33a8406eba93324164

--- hwjob/hwjob/settings.py
+++ hwjob/hwjob/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "dashboard",
+    "dashboard.apps.DashboardConfig",
     "dashboard.apps.DashboardAdminConfig",
 ]
 
